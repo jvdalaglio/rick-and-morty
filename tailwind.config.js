d94d1daf6a209fac0d20041169b365e554227472
jvdalaglio <jvdalaglio@gmail.com>
2025-01-09 05:37:15 -0300

    correções

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,11 +15,13 @@ module.exports = {
         'core-content-low': '#A4A4A4',
         'neutral-content': '#A4A4A4',
         'neutral-surface': '#FFFFFF',
-        'neutral-content-onSource': '#0A0A0A'
+        'neutral-content-onSource': '#0A0A0A',
+        'core-border': '#3D3D3D'
       },
       fontFamily: {
         creepster: ['Creepster', 'serif'],
       },
+
     },
   },
   plugins: [],
